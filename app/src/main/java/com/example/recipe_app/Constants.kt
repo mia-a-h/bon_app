f1033e289c662c8528d60ab7c711e2b82a18d2ae
DESKTOP-1IVE5MC\Mia <miaah2004@gmail.com>
2025-01-04 14:04:43 +0200

    minor changes

--- app/src/main/java/com/example/recipe_app/Constants.kt
+++ app/src/main/java/com/example/recipe_app/Constants.kt
@@ -4,4 +4,5 @@ object Constants {
     //const val API_KEY = "71f1502f6faf4eacb45b78e507889679" //my key
     const val API_KEY = "a54df63c616c440887fd9cc05371aac8" //my 2nd key
     //const val API_KEY = "9b31a6de51e34ce89699ac0e7ac03199"
+    //const val API_KEY = "d9b81fe885384f47ac838c6f198370a3"
 }
