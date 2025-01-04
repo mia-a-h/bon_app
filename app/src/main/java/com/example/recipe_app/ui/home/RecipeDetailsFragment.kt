1b36c6b0196771ad08520584fbcc327d869abc1a
DESKTOP-1IVE5MC\Mia <miaah2004@gmail.com>
2025-01-04 14:04:28 +0200

    minor changes

--- app/src/main/java/com/example/recipe_app/ui/home/RecipeDetailsFragment.kt
+++ app/src/main/java/com/example/recipe_app/ui/home/RecipeDetailsFragment.kt
@@ -8,11 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
